ef0e34ddf582b5a4692ed19828cea8f985ae6432
Zhang Lei <858074256@qq.com>
2022-06-03 14:48:57 +0800

    更改为多文件上传

--- controller/add.go
+++ controller/add.go
@@ -11,14 +11,19 @@ import (
 
 // Add 上传照片，照片存储到/public/pic中
 func Add(c *gin.Context) {
-	file, _ := c.FormFile("image")
+	form, _ := c.MultipartForm()
 
-	saveName := strconv.Itoa(int(time.Now().Unix())) + file.Filename
-	Log.Println("存储：", saveName)
+	files := form.File["image"]
+	var saveName []string
+	for i, file := range files {
+		saveName = append(saveName, strconv.Itoa(int(time.Now().Unix()))+file.Filename)
+		Log.Println("存储：", saveName[i])
 
-	dst := "./public/pic/" + saveName
-	// 上传文件至指定的完整文件路径
-	c.SaveUploadedFile(file, dst)
+		dst := "./public/pic/" + saveName[i]
+		// 上传文件至指定的完整文件路径
+		c.SaveUploadedFile(file, dst)
+	}
+
 	f, err := os.OpenFile("./public/pic/pic.xml", os.O_RDWR, 0666)
 
 	if err != nil {
@@ -30,20 +35,23 @@ func Add(c *gin.Context) {
 	}
 	f.Close()
 
-	f, err = os.OpenFile("./public/pic/pic.xml", os.O_CREATE|os.O_APPEND|os.O_WRONLY|os.O_RDWR, 6060)
+	f, err = os.OpenFile("./public/pic/pic.xml", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 6060)
 
 	if err != nil {
 		Log.Println("打开pic.xml文件失败：", err)
 	}
 
-	var strings = []string{
-		"<photo>" + saveName + "</photo>",
-		"</pic>",
+	var strings []string
+
+	for _, name := range saveName {
+		strings = append(strings, "<photo>"+name+"</photo>")
 	}
+	strings = append(strings, "</pic>")
 	err = addLine(f, strings)
 	if err != nil {
 		Log.Println("添加文档失败", err)
 	}
 	f.Close()
-	c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
+
+	c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", saveName))
 }
